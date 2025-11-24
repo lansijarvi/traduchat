49e9ce160bff5d5b335a355d5f5e305f0d65e2a1
Sean Westlake <honda450@gmail.com>
2025-11-24 01:29:33 +0000

    gs://studio-1352566969-d9907.firebasestorage.app

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: 'AIzaSyAn110Yl4-k7pXpRuiJ0KD9W1GluJjk52Q',
   authDomain: 'studio-1352566969-d9907.firebaseapp.com',
   projectId: 'studio-1352566969-d9907',
-  storageBucket: 'studio-1352566969-d9907.appspot.com',
+  storageBucket: 'studio-1352566969-d9907.firebasestorage.app',
   messagingSenderId: '688606062258',
   appId: '1:688606062258:web:e78e4cbed14d6dbe950bab',
 };
